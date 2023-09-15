4424fd10e02a696a709ec44b48b3dcde43605981
Deus <amadeusrm97@gmail.com>
2023-09-15 08:47:49 -0400

    added scroll handling logic

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,29 @@
+import { useEffect, useState } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 export const NavBar = () => {
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  });
+  
   return (
     <Navbar expand="lg">
       <Container>
         <Navbar.Brand href="#home">
-          <img src={''} alt="Logo"/>
+          <img src={""} alt="Logo" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" >
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
           <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
@@ -16,11 +33,19 @@ export const NavBar = () => {
             <Nav.Link href="#projects">Projects</Nav.Link>
             <span className="navbar-text">
               <div className="social-icon0">
-                <a href="#home"><img src={''} alt="" /></a>
-                <a href="#home"><img src={''} alt="" /></a>
-                <a href="#home"><img src={''} alt="" /></a>
+                <a href="#home">
+                  <img src={""} alt="" />
+                </a>
+                <a href="#home">
+                  <img src={""} alt="" />
+                </a>
+                <a href="#home">
+                  <img src={""} alt="" />
+                </a>
               </div>
-              <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
+              <button className="vvd" onClick={() => console.log("connect")}>
+                <span>Let's Connect</span>
+              </button>
             </span>
           </Nav>
         </Navbar.Collapse>
